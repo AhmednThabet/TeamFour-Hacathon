335cdf6e97f2b4e00f9db9586c4273ef34a6da32
Ahmed Thabet <ah12.9.2000@gmail.com>
2023-03-14 21:11:03 +0200

    Revert "new Folder Structure"
    This reverts commit f16bf0fedb40ba5daf78566706728ce51ce522c7.

--- src/pages/teamFour/Balance/bank/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Banck = () => {
-  return <div>Banck</div>;
-};
-
-export default Banck;
--- src/pages/teamFour/Balance/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WithDraow = () => {
-  return <div>WithDraow</div>;
-};
-
-export default WithDraow;
